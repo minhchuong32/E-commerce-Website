cc93c6157b267823a5937f99dc24946f63f1283f
minhchuong32 <chuongminh3225@gmail.com>
2025-06-22 23:02:12 +0700

    Order policy

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
 import BestSeller from "../components/BestSeller";
+import OrderPolicy from "../components/OrderPolicy";
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
       <BestSeller />
+      <OrderPolicy />
     </div>
   );
 };
