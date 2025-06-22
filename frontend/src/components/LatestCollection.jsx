0f9fd1a6320c5e53ce2446a33cb7a0e3a27286ab
minhchuong32 <chuongminh3225@gmail.com>
2025-06-22 21:47:13 +0700

    title

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,5 +1,7 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import {ShopContext} from '../context/ShopContext'
+import Title from './Title'
+
 const LatestCollection = () => {
 
     const { products } = useContext(ShopContext);
@@ -8,7 +10,14 @@ const LatestCollection = () => {
 
 
   return (
-    <div>LatestCollection</div>
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+            <Title text1={'LATEST'} text2={'COLLECTION'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, in?
+        </p>
+        </div>
+    </div>
   )
 }
 
