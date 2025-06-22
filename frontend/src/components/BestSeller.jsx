5541740b1cf66970c6f27eae88817227d9cdc3f5
minhchuong32 <chuongminh3225@gmail.com>
2025-06-22 22:54:19 +0700

    render bestseller product

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, useState, useEffect } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const BestSeller = () => {
+  const { products } = useContext(ShopContext);
+  const [bestSellers, setBestSellers] = useState([]);
+
+  useEffect(() => {
+    // Filter products to find bestsellers
+    const bestProduct = products.filter((item) => item.bestseller === true);
+    setBestSellers(bestProduct.slice(0, 5)); // Get the first 5 bestsellers
+  }, []);
+  return (
+    <div className="my-10">
+      {/* Best Sellers Title */}
+      <div className="text-center text-3xl py-8">
+        <Title text1={"BEST"} text2={"SELLERS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, aut!
+        </p>
+      </div>
+
+      {/* Best Sellers Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 gap-y-6">
+        {bestSellers.map((item, index) => {
+          return (
+            <ProductItem
+              key={index}
+              id={item._id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
