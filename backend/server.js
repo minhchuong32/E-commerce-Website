637e1e3b90903af926a9957dc8bfc288268ba769
minhchuong32 <chuongminh3225@gmail.com>
2025-07-19 20:31:51 +0700

    init backend folder and connect mongoose db

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+
+// App config
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
+
+// middleware 
+app.use(express.json())
+app.use(cors())
+
+// api endpoints 
+app.get('/', (req, res) => {
+    res.send("API working")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
