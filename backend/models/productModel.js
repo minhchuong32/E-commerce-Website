0640de78d38f4401a063e7ef8af43555a09e3bbc
minhchuong32 <chuongminh3225@gmail.com>
2025-07-22 15:00:42 +0700

    done productController

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
     name: {type:String, required: true},
     description: {type:String, required: true},
     price: {type:Number, required: true},
-    image: {type:String, required: true},
+    images: [{ type: String, required: true }],
     category: {type:String, required: true},
     subCategory: {type:String, required: true},
     sizes: {type:Array, required: true},
@@ -12,6 +12,6 @@ const productSchema = new mongoose.Schema({
     date: {type:Number, default: Date.now},
 })
 
-const Product = mongoose.models.product ||  mongoose.model('Product', productSchema);
+const productModel = mongoose.models.product ||  mongoose.model('Product', productSchema);
 
-export default Product;
+export default productModel;
