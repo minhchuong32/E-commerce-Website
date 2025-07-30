6577f959e444730674d41fe31edaae35af40e151
minhchuong32 <chuongminh3225@gmail.com>
2025-07-30 21:12:15 +0700

    fix file vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {port : 5174},
-   plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {port : 5174}
 })
